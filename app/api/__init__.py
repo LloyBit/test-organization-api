8d3d0ec5603d2e012473f626de92f5dde8f6042c
Dmitriy <Krivopusk.mitya@gmail.com>
2025-10-26 01:40:51 +0300

    delete routers unificator

--- app/api/__init__.py
+++
@@ -1,9 +0,0 @@
-''' Модуль для объединения всех роутов '''
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/")
-async def root():
-    """ Главная страница """
-    return {"message": "Main page"}
